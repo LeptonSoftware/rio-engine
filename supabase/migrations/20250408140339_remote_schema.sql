f85ee61e029f63096e1ca9257780590e98cb291d
imran <imran.ansari@leptonsoftware.com>
2025-04-08 19:35:41 +0530

    migration

---
+++ supabase/migrations/20250408140339_remote_schema.sql
@@ -0,0 +1,7 @@
+alter table "public"."workflows" drop constraint "workflows_status_check";
+
+alter table "public"."workflows" add constraint "workflows_status_check" CHECK (((status)::text = ANY ((ARRAY['active'::character varying, 'archived'::character varying, 'draft'::character varying])::text[]))) not valid;
+
+alter table "public"."workflows" validate constraint "workflows_status_check";
+
+
